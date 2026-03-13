a2185d6a9846ca5b880fd2e899301a64e1d991d6
juliakosloff <juliakosloff@seattleacademy.org>
2026-03-13 21:08:57 +0000

    test

---
+++ jokes.py
@@ -0,0 +1,29 @@
+print("What do you call a fake noodle?")
+input()
+print("An impasta!")
+print("Click ENTER for antoher joke.")
+input()
+print("What do you call a shoe that is made by a banana?")
+input()
+print("A slipper!")
+print("Click ENTER for another joke.")
+input()
+print("Knock Knock")
+input()
+print("Who is there?")
+input()
+print("Boo")
+input()
+print("Boo who?")
+input()
+print("Why are you crying? Its just a joke")
+
+
+
+
+
+
+
+
+
+
